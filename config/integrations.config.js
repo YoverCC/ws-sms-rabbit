f529a82dbf49ec0cbbd4c834fa5891058004bbb9
YoverCC <ycastroc@uni.pe>
2024-01-16 19:58:54 +0000

    Readme added, config Rabbit host added

--- config/integrations.config.js
+++ config/integrations.config.js
@@ -3,6 +3,7 @@ exports.SMS_QUEUE_STATUS = 'SEND'
 exports.SMS_FAILED_STATUS = 'FAIL'
 
 // Datos para Rabbit
+exports.RABBIT_SERVER = 'amqp://localhost'
 exports.RABBIT_QUEUE = 'sms_flujos'
 exports.RABBIT_SETTINGS_PERSISTENT = true
 exports.RABBIT_SETTINGS_DURABLE = true
--- shared/rabbitmq.shared.js
+++ shared/rabbitmq.shared.js
@@ -3,6 +3,7 @@ let ampq = require('amqplib');
 let log4js = require("log4js");
 
 // Import config
+let RABBIT_SERVER = require("../config/integrations.config").RABBIT_SERVER;
 let RABBIT_QUEUE = require("../config/integrations.config").RABBIT_QUEUE;
 let RABBIT_SETTINGS_PERSISTENT = require("../config/integrations.config").RABBIT_SETTINGS_PERSISTENT;
 let RABBIT_SETTINGS_DURABLE = require("../config/integrations.config").RABBIT_SETTINGS_DURABLE;
@@ -25,7 +26,7 @@ class RabbitService {
 
     try {
       // Rabbit process
-      connection = await ampq.connect("amqp://localhost");
+      connection = await ampq.connect(RABBIT_SERVER);
       const channel = await connection.createChannel();
 
       await channel.assertQueue(RABBIT_QUEUE,
